465d7c692d2f3f6fb9d2bfc70bb58e3532ab9ca1
noam <noamhaner@gmail.com>
2024-02-01 14:33:30 +0200

    created equipment # still needs constractors

--- GridGame/Services/Equipment.cs
+++ GridGame/Services/Equipment.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace GridGame.Services
 {
-    public class Equipment
+    public class Equipment//what is equiped
     {
+        public Tag Weapon {  get; set; }
+        public Tag Armour { get; set; }
+        public Tag Ring { get; set; }
+
+    }
+
+    public class Tag//in inventory and not equiped equipment
+    {
+        public string name {  get; set; }
+        public string description {  get; set; }
+        public int weaponStr {  get; set; }
+        public int armourDef {  get; set; }
+        public Stats RingStats {  get; set; }
+
     }
 }
