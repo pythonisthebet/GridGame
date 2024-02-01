459483ec43b2d9b3becfdb7c1d9a828e605eeec1
noam <noamhaner@gmail.com>
2024-02-01 14:25:14 +0200

    made constrators for stats

--- GridGame/Services/Stats.cs
+++ GridGame/Services/Stats.cs
@@ -8,5 +8,14 @@ namespace GridGame.Services
 {
     public class Stats
     {
+        public int Strength {  get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence {  get; set; }
+        public int Luck {  get; set; }
+        public int Charm {  get; set; }
+        public static Random Random = new Random();
+        public Stats() { Strength = Random.Next(1, 3); Dexterity = Random.Next(1, 3); Intelligence = Random.Next(1, 3); Luck = Random.Next(1, 3); Charm = 15 - (Strength + Dexterity + Intelligence + Luck); }//new cheracter
+        public Stats(int str,int dex, int intel,int luk, int cha) { Strength = str; Dexterity = dex; Intelligence = intel; Luck = luk; Charm = cha; }//new companion/monster
+
     }
 }
